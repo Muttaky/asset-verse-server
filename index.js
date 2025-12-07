ab18aff4b77a6ff44b66415b3b1a8890a909d00f
Muttaky <muttaky9@gmail.com>
2025-12-07 20:59:48 +0600

    AssetVerseDB implemented

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const AssetVerseDB = client.db("AssetVerseDB");
+
+    const userCol = AssetVerseDB.collection("users");
+
+    //CREATE
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await userCol.insertOne(newUser);
+      res.send(result);
+    });
+
+    //READ
+    app.get("/users", async (req, res) => {
+      const cursor = userCol.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
