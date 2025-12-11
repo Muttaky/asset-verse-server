8181b0093f9732e1c980f426e2ef77f56f5bbf58
Muttaky <muttaky9@gmail.com>
2025-12-12 05:04:54 +0600

    JWT implemented

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require(process.env.SERVICE_SECRET);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
